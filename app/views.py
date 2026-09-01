8c4f5bbe386f9e97c7b2c48834af605e1506ec55
DiegoFernandez-DF <diegoestefer@gmail.com>
2026-08-31 23:30:23 -0300

    feat: implementación de autenticación y navbar dinamica

--- app/views.py
+++ app/views.py
@@ -1,25 +1,31 @@
 """Vistas iniciales para navegar médicos y pantalla de inicio."""
 
-from django.views.generic import ListView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, TemplateView, DetailView
 from .models import Medico
 
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin ,TemplateView):
     """Vista de inicio. Por ahora vacía — completar con estadísticas."""
 
     template_name = "clinica/home.html"
 
 
-class ListaMedicosView(ListView):
+class ListaMedicosView(LoginRequiredMixin, ListView):
     """Lista todos los médicos."""
 
     model = Medico
     template_name = "clinica/lista_medicos.html"
     context_object_name = "medicos"
 
+class DetalleMedicoView(LoginRequiredMixin, DetailView):
+    """Muestra los detalles de un médico."""
+
+    model = Medico
+    template_name = "clinica/detalle_medico.html"
+    context_object_name = "medico"
 
 # TODO: implementar las siguientes vistas:
-# class DetalleMedicoView(...): ...
 # class ListaTurnosView(...): ...
 # class NuevoTurnoView(...): ...
 # class CancelarTurnoView(...): ...
